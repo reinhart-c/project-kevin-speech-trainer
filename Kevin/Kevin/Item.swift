a28daa19cdfcec2f85357296b08433eb85bfc535
Teuku Fazariz Basya <teukufazarizbasya@gmail.com>
2025-06-10 13:54:08 +0700

    Base App

---
+++ Kevin/Kevin/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Kevin
+//
+//  Created by Teuku Fazariz Basya on 10/06/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Kevin/Kevin/KevinApp.swift
@@ -0,0 +1,32 @@
+//
+//  KevinApp.swift
+//  Kevin
+//
+//  Created by Teuku Fazariz Basya on 10/06/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct KevinApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
