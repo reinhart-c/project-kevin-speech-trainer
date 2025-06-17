894878f34fd3a33e2cafe3d4edcb03fa392abf90
laleaff <liffa.rr@gmail.com>
2025-06-17 15:01:53 +0700

    fix cardView

--- Kevin/Homepage/View/CategoryCardListView.swift
+++ Kevin/Homepage/View/CategoryCardListView.swift
@@ -9,17 +9,38 @@ import SwiftUI
 struct CategoryCardListView: View {
     @StateObject private var viewModel = CategoryViewModel()
     @Binding var path: NavigationPath
+    @State private var showModal = false
+    @State private var selectedCategory: Category? = nil
 
     var body: some View {
         HStack(alignment: .top, spacing: 16) {
             CategoryCardView(category: viewModel.categories[0], path: $path)
-                .frame(width: 440, height: 500)
+                .frame(width: 445, height: 500)
+                .onTapGesture {
+                    selectedCategory = viewModel.categories[0]
+                    showModal = true // Show modal on tap
+                }
             CategoryCardView(category: viewModel.categories[1], path: $path)
-                .frame(width: 440, height: 500)
+                .frame(width: 445, height: 500)
+                .onTapGesture {
+                    selectedCategory = viewModel.categories[1]
+                    showModal = true
+                }
             CategoryCardView(category: viewModel.categories[2], path: $path)
-                .frame(width: 440, height: 500)
+                .frame(width: 445, height: 500)
+                .onTapGesture {
+                    selectedCategory = viewModel.categories[2]
+                    showModal = true
+                }
         }
         .padding()
+        .sheet(isPresented: $showModal) {
+            if let category = selectedCategory {
+                CategoryModalView(category: category) {
+                    path.append("SpeechView")
+                }
+            }
+        }
     }
 }
 
